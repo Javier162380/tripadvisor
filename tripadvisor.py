cbd8ef6a328eec7448a777ea22e04c36d6aff61c
Javier Llorente <javierllorente@MacBook-Air-de-Javier.local>
2017-10-15 23:22:18 +0200

    Pequeños cambios, tratando de mejor el script, expresiones regulares y cambio la primera funcion para evitar request tontas

--- tripadvisor.py
+++ tripadvisor.py
@@ -1,26 +1,27 @@
 
-
+from requests import get
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re 
 from time import sleep
 from sys import argv
 
-#we get all the different pages were we can see a list of hotels.
+#we get all the different pages were we can see a list of all the hotels.
 def internallinks(url, n):
 
-	linkslist=[]
-	request=urlopen(url)
-	parser=BeautifulSoup(request,'lxml')
+	linkslist=set()
+	request=get(url)
+	parser=BeautifulSoup(request.text,'lxml')
 	for link in parser.find(class_="pageNumbers").findAll("a", href=re.compile("^(/|.*)")):
 		if link.attrs['href'] is not None:
 			listurl=link.attrs['href']
 			link='https://www.tripadvisor.es'+str(listurl)
-			linkslist.append(link)
+			linkslist.add(link)
 		else:
 			pass
-	urllist=[]
+	urllist=set()
 	while n>0:
+		pageslinks=set()
 		for i in linkslist:
 			request=urlopen(i)
 			parser=BeautifulSoup(request,'lxml')
@@ -28,51 +29,53 @@ def internallinks(url, n):
 				if link.attrs['href'] is not None:
 					listurl=link.attrs['href']
 					link='https://www.tripadvisor.es'+str(listurl)
-					urllist.append(link)
+					if link not in urllist:
+						urllist.add(link)
+						pageslinks.add(link)
+					elif link not in pageslinks:
+						pass
+					else:
+						pageslinks.remove(link)
 				else:
 					pass
-		linkslist=set(urllist)
+		linkslist=set(pageslinks)
 		n=n-1
 	return set(urllist)
 
 #we get all the individual links of all the hotels
 def gethotelslinks(urllist):
 
-	hotelsList=[]
+	hotelsList=set()
 	for i in urllist:
 		sleep(1)
-		request=urlopen(i)
-		parser=BeautifulSoup(request,'lxml')
-		for link in parser.findAll("a", href=re.compile("^(/|.*)")):
+		request=get(i)
+		parser=BeautifulSoup(request.text,'lxml')
+		for link in parser.findAll("a", href=re.compile("^(/|.*)(?=REVIEWS)")):
 			if link.attrs['href'] is not None:
 				hotelurl=link.attrs['href']
-				if 'Hotel_Review' in hotelurl and '#REVIEWS' not in hotelurl:
-					url='https://www.tripadvisor.es'+str(hotelurl)
-					hotelsList.append(url)
-				else:
-					pass
+				url='https://www.tripadvisor.es'+str(hotelurl)
+				print(url)
+				hotelsList.add(url)
 			else:
 				pass
-	return set(hotelsList)
+	return hotelsList
 
 def gethotelslinknopages(url):
 
-	hotelsList=[]
-	request=urlopen(url)
-	parser=BeautifulSoup(request,'lxml')
-	for link in parser.findAll("a", href=re.compile("^(/|.*)")):
+	hotelsList=set()
+	request=get(url)
+	parser=BeautifulSoup(request.text,'lxml')
+	for link in parser.findAll("a", href=re.compile("^(/|.*)(?=REVIEWS)")):
 		if link.attrs['href'] is not None:
 			hotelurl=link.attrs['href']
-			if 'Hotel_Review' in hotelurl and '#REVIEWS' not in hotelurl:
-				url='https://www.tripadvisor.es'+str(hotelurl)
-				hotelsList.append(url)
-			else:
-				pass
+			url='https://www.tripadvisor.es'+str(hotelurl)
+			print(url)
+			hotelsList.add(url)
 		else:
 			pass
 	return set(hotelsList)
 
-#we extract the date we want from the hotels in this case the number of rooms and name of  the hotel .
+#we extract the data we want from the hotels in this case the number of rooms and name of  the hotel .
 def gethotelsinfo(hotelsList):
 
 	hotelsinformation=[]
@@ -80,24 +83,22 @@ def gethotelsinfo(hotelsList):
 	for i in hotelsList:
 		info={}
 		sleep(1)
-		request=urlopen(i)
-		parser=BeautifulSoup(request,'lxml')
-		
+		request=get(i)
+		parser=BeautifulSoup(request.text,'lxml')
 		if str(type(parser.find(class_="list number_of_rooms")))!="<class 'NoneType'>":
-			numberofrooms=parser.find(class_="list number_of_rooms").get_text().split('s')[1].split(' ')[0]
-			
+			numberofrooms=parser.find(class_="list number_of_rooms").get_text(';').split(';')[1]
 		else:
-			pass
+			numberofrooms=None
 		if type(parser.find(class_="heading_title"))!=None:
 			name=parser.find(class_="heading_title").get_text()
 			title=re.sub('\n','',name)
 		else:
-			pass 
+			title=None 
 		info[title]=numberofrooms
 		hotelsinformation.append(info)
 	return hotelsinformation
+	
 #We need to insert the url complusory, the number of pages we want to retrieve it is optional.	
-
 def main():
 
 	
