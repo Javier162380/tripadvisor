7a56c0bcc718b6fb4feb7c91a894750b8f7f1048
Javier Llorente <javierllorente@MacBook-Air-de-Javier.local>
2017-10-21 13:18:19 +0200

    cambios para contemplar todas las excepciones que se me ocurren

--- postgre.py
+++ postgre.py
@@ -5,65 +5,91 @@ import psycopg2
 class postgre():
     '''This class it is just performed for execute multiple row insert in 
     PostgreSQL'''
-    def __init__(self,user,password,host,dbname):
-        self.user=user
+    def __init__(self,username,password,host,dbname):
+        self.username=username
         self.password=password
         self.host=host
         self.dbname=dbname
         
     def connect(self):
         try:
-            conn = psycopg2.connect(dbname=self.dbname,user=self.user,
+            conn = psycopg2.connect(dbname=self.dbname,user=self.username,
                                     password=self.password)
             return conn
         except psycopg2.Error as e:
-            raise psycopg2.Error("Hemos registrado el siguiente error {0}"
+            raise psycopg2.Error("Hemos registrado el siguiente error: {0}"
                             .format(str(e))) 
      
-    def execute_multiple_inserts(data,table,chunksize,conn):
+    def execute_multiple_inserts(self,data,table,chunksize,conn):
         #we create a cursor 
         try:
            cur = conn.cursor()
         except psycopg2.Error as e:
-            raise psycopg2.Error("Hemos registrado el siguiente error {0}"
-            .format(e))    
+            raise psycopg2.Error("Hemos registrado el siguiente error: {0}"
+                .format(e))    
         #we convert the data into a tuple of tuples.
         if type(data) is list:
-            if type(data[0]) is list:
-                clean_data=tuple(tuple(i) for i in data)
-            elif type(data[0]) is tuple:
-                clean_data=tuple(data)
+            if len(data)>0:
+                if type(data[0]) is list:
+                    clean_data=tuple(tuple(i) for i in data)
+                elif type(data[0]) is tuple:
+                    clean_data=tuple(data)
+                else:
+                    raise ValueError("formato de datos no adecuado.formato adecuado: "
+                        +"Lista de registros,lista de listas,lista de tupla,tupla de " 
+                        +"registros,tupla de listas o tupla de tuplas")
+            #Just in case we only want to insert one register per row
             else:
-                raise ValueError("formato de datos no adecuado.formato adecudo:"
-                    +"Lista de listas,lista de tupla,tupla de listas o tupla"+ 
-                    +"tuplas")
+                cleandata=tuple(tuple(i) for i in data)
         elif type(data) is tuple:
-            if type(data[0]) is list:
-                clean_data=[tuple(i) for i in data]
-            elif type(data[0]) is tuple:
-                clean_data=data
+            if len(data)>0:
+                if type(data[0]) is list:
+                    clean_data=tuple(tuple(i) for i in data)
+                elif type(data[0]) is tuple:
+                    clean_data=data
+                else:
+                    raise ValueError("formato de datos no adecuado.formato adecuado: "
+                        +"Lista de registros,Lista de listas,lista de tupla,tupla de " 
+                        +"registros,tupla de listas o tupla de tuplas")
             else:
-                raise ValueError("formato de datos no adecuado.formato adecudo:"
-                    +"Lista de listas,lista de tupla,tupla de listas o tupla"+
-                   +"de tuplas")
+                #Just in case we only want to insert one register per row                
+                cleandata=tuple(tuple(i) for i in data)
         else:
-            raise ValueError("formato de datos no adecuado.formato adecudo:"
-               +"Lista de listas,lista de tupla,tupla de listas o tupla de"+
-               +"tuplas")
+            raise ValueError("formato de datos no adecuado.formato adecuado: "
+               +"Lista de registros,lista de listas,lista de tupla,tupla de registros,"
+               +"tupla de listas o tupla de tuplas.")
         #we split the tuple into smaller tuples.
         insert_data,clean_data=clean_data[:chunksize],clean_data[chunksize:] 
         #we execute the insert statement.
-        while len(clean_data)>0:
-            str_insert=str(insert_data)[1:-1]
+        #we create a flag for the case chunksize>cleandata.
+        flag=0
+        if flag==0:
+            str_insert=str(insert_data)
+            if str_insert[-2]==",":
+                str_insert=str_insert[1:-2]
+            else:
+                str_insert=str_insert[1:-1]
             try:
-                cur.execute("INSERT INTO "+str(table)+
-                            " VALUES "+str(str_insert)+";" )
+                ex=cur.execute("INSERT INTO "+str(table)+
+                               " VALUES "+str(str_insert)+";" )
             except Exception as e:
-                print("error durante la inserció0".format(e))
+                   raise Exception("error durante la inserción  {}".format(e))
+            flag=1            
+        else:
+            while len(clean_data)>0:
+                str_insert=str(insert_data)
+                if str_insert[-2]==",":
+                    str_insert=str_insert[1:-2]
+                else:
+                    str_insert=str_insert[1:-1]
+                try:
+                    ex=cur.execute("INSERT INTO "+str(table)+
+                                " VALUES "+str(str_insert)+";" )
+                except Exception as e:
+                    raise Exception("error durante la inserción {e}".format(e))
+                    break
         #we commit the insert 
         conn.commit()
         #we close the cursor and connection.
         cur.close()
         conn.close()
-a=postgre('pablo','es','muy','tonto')
-a.connect()
