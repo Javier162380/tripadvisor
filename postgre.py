bf9b32037e3c1c6901337d06283b5f913121e6da
Javier162380 <javierllorente16@gmail.com>
2017-10-24 00:42:57 +0200

    Añado una función  a la clase para leer queries de postgre y convertirlas directamente en un data frame

--- postgre.py
+++ postgre.py
@@ -2,11 +2,15 @@
 
 import psycopg2
 import credentials
+from pandas import DataFrame
+
 class postgre():
-    '''This class it is just performed for execute multiple row insert in 
+
+    '''This class it is just performed for execute multiple row insert in
     PostgreSQL'''
-    def __init__(self,username=credentials.username,
-                password=credentials.password,dbname=credentials.dbname):
+
+    def __init__(self, username=credentials.username ,
+                password=credentials.password , dbname=credentials.dbname):
         self.username=credentials.username
         self.password=credentials.password
         self.dbname=credentials.dbname
@@ -20,7 +24,7 @@ class postgre():
             raise psycopg2.Error("Hemos registrado el siguiente error: {0}"
                             .format(str(e))) 
      
-    def execute_multiple_inserts(self,data,table,chunksize,conn):
+    def execute_multiple_inserts(self, data, table, chunksize,conn):
         #we create a cursor 
         try:
            cur = conn.cursor()
@@ -41,7 +45,7 @@ class postgre():
                         +"registros,tupla de listas o tupla de tuplas.")
             #Just in case we only want to insert one register per row
             else:
-                cleandata=tuple(tuple(i) for i in data)
+                clean_data=tuple(tuple(i) for i in data)
         elif type(data) is tuple:
             if len(data)>0:
                 if type(data[0]) is list:
@@ -54,7 +58,7 @@ class postgre():
                         +"registros,tupla de listas o tupla de tuplas.")
             else:
                 #Just in case we only want to insert one register per row                
-                cleandata=tuple(tuple(i) for i in data)
+                clean_data=tuple(tuple(i) for i in data)
         else:
             raise ValueError("formato de datos no adecuado.formato adecuado: "
                +"Lista de registros,lista de listas,lista de tupla,tupla de registros,"
@@ -64,7 +68,6 @@ class postgre():
 
         if chunksize>len(clean_data):
             number_of_records = ','.join(['%s'] * len(clean_data))
-            print(number_of_records)
             try:
                 insert_statement="INSERT INTO "+str(table)+" VALUES {0}".format(number_of_records)
                 cur.execute(insert_statement,clean_data)
@@ -86,3 +89,30 @@ class postgre():
         #we close the cursor and connection.
         cur.close()
         conn.close()
+
+    def postre_to_dataframe(self, query, conn):
+
+        # we create a cursor
+        try:
+            cur = conn.cursor()
+        except psycopg2.Error as e:
+            raise psycopg2.Error("Hemos registrado el siguiente error: {0}"
+                                 .format(e))
+
+        # we execute the query statement.
+        try:
+            cur.execute(query)
+            results = cur.fetchall()
+            # we get the information about the columns names.
+            columns_names = [i[0] for i in cur.description]
+
+        except psycopg2.Error as e:
+            raise psycopg2.Error("Hemos registrado el siguiente error: {0}"
+                                 .format(e))
+
+        # we close the cursor and connection.
+        cur.close()
+        conn.close()
+
+        # we create the data frame.
+        return DataFrame.from_records(results, columns=columns_names)
